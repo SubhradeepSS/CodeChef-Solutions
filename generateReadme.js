0d187b12553ebda6eb8863ea67d9f9ee6e405da6
Subhradeep Saha <subhradeep_saha@outlook.com>
2021-08-09 12:59:14 +0530

    📝docs: added icons for ci status and languages

--- generateReadme.js
+++ generateReadme.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
-const ignore_files = ['.git', '.gitignore', '.vscode', 'README.md', 'generateReadme.js', 'DSA Series']
+const ignore_files = ['.git', '.gitignore', '.vscode', 'README.md', 'generateReadme.js', 'DSA Series', '.github', 'scripts']
 const codechef_url = "https://www.codechef.com"
 const repo_url = "https://github.com/SubhradeepSS/CodeChef-Solutions/tree/master"
 
-let readme = "# CodeChef Solutions\n\n | Contest | Problem | Solution |\n| --- | --- | --- |\n"
+let readme = `# CodeChef Solutions\n![C Compilation Test](https://github.com/SubhradeepSS/CodeChef-Solutions/actions/workflows/c-compile-test.yml/badge.svg) ![C++ Compilation Test](https://github.com/SubhradeepSS/CodeChef-Solutions/actions/workflows/cpp-compile-test.yml/badge.svg) ![Java Compilation Test](https://github.com/SubhradeepSS/CodeChef-Solutions/actions/workflows/java-compile-test.yml/badge.svg) ![Python Syntax Check Test](https://github.com/SubhradeepSS/CodeChef-Solutions/actions/workflows/python-syntax-check.yml/badge.svg)\n
+![](https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white) ![](https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white) ![](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white) ![](https://img.shields.io/badge/python-%2314354C.svg?style=for-the-badge&logo=python&logoColor=white)\n\n | Contest | Problem | Solution |\n| --- | --- | --- |\n`
 
 fs.readdir(__dirname, (err, contests) => {
     if (err) {
